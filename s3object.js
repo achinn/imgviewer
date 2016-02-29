867a5859871bfaf19ddf02d7d0394093d0b53218
Alex Chinn <alex.chinn@d2l.com>
2016-02-28 22:43:07 -0500

    Directories can be drilled into and images can be viewed. Still need to add going up dirs.

--- s3object.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class S3Object extends React.Component {
-	render() {
-		return <li>{this.props.name}</li>;
-	}
-}
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+	accessKeyId: null,
+	secretAccessKey: null,
+	bucketName: null
+};
+
+export default config;
